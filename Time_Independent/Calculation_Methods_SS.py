f3d058aefe15827716365a48faf33be1d1d6988e
BadCodeLife <35963779+BadCodeLife@users.noreply.github.com>
2019-04-14 00:55:37 +0100

    Setting up steady state to use shifting states

--- Time_Independent/Calculation_Methods_SS.py
+++ Time_Independent/Calculation_Methods_SS.py
@@ -6,24 +6,25 @@ import copy
 
 Gate_name = 'Gate_(e)'
 Bias_name = 'Bias_(eV)'
-States_name = 'States Set'
+States_name = 'States Range'
 Thermal_E_name = 'Thermal_Energy_(Delta)'
 Leak_name = 'Leakage_(Delta)'
 Charge_E_name = 'Charging_Energy_(Delta)'  # charging energy of the island
 Super_Con_name = 'Superconducting'
 Current_name = 'Current'
 
+
 # Method that makes a more formal definition of what the expected dictionary for a data point should be.
 # This is an incomplete DataPoint as it does not include the expectation value of current 'Current'
-def create_datapoint_dict_steady(gate,bias,n_set,temp,leak,charge_energy,super):
+def create_datapoint_dict_steady(gate, bias, n_set, temp, leak, charge_energy, super):
     dict = {
-        Gate_name:gate,
-        Bias_name:bias,
-        States_name:n_set,
-        Thermal_E_name:temp,
-        Leak_name:leak,
+        Gate_name: gate,
+        Bias_name: bias,
+        States_name: n_set,
+        Thermal_E_name: temp,
+        Leak_name: leak,
         Charge_E_name: charge_energy,
-        Super_Con_name:super
+        Super_Con_name: super
     }
     return dict
 
@@ -31,50 +32,60 @@ def create_datapoint_dict_steady(gate,bias,n_set,temp,leak,charge_energy,super):
 ########################################################################################################################
 # calculations
 
+
+def vector_base(gate, limit):
+    center = - int(round(gate))
+    return sp.arange(center - limit, center + limit + 1)
+
+
 # calculates the
-def create_matrix(gate,bias,n_set):
+def create_matrix(gate, bias, n_set):
     dim = len(n_set)
-    matrix = sp.zeros((dim,dim))
+    matrix = sp.zeros((dim, dim))
     for i in xrange(dim):
-        if i<dim-1:
-            matrix[i][i] = su.tunnel_on(gate,bias,n_set[i])
-            matrix[i][i+1] = -su.tunnel_off(gate,bias,n_set[i+1])
-        matrix[dim-1][i] = 1
+        if i < dim - 1:
+            matrix[i][i] = su.tunnel_on(gate, bias, n_set[i])
+            matrix[i][i + 1] = -su.tunnel_off(gate, bias, n_set[i + 1])
+        matrix[dim - 1][i] = 1
     return matrix
 
 
-def probability_calc(gate,bias,n_set):
+def probability_calc(gate, bias, n_set):
     dim = len(n_set)
     y = sp.zeros(dim)
-    y[dim-1]=1
-    matrix = create_matrix(gate,bias,n_set)
-    out = sp.linalg.solve(matrix,y)
+    y[dim - 1] = 1
+    matrix = create_matrix(gate, bias, n_set)
+    out = sp.linalg.solve(matrix, y)
     return out
 
 
-def tunnel_vector(gate,bias,n_set):
+def tunnel_vector(gate, bias, n_set):
     out = sp.array([])
     for n in n_set:
-        rate = su.tunnel_flow_j1(gate_charge=gate,bias_potential=bias,extra_electrons=n)
-        out = sp.append(out,rate)
+        rate = su.tunnel_flow_j1(gate_charge=gate, bias_potential=bias, extra_electrons=n)
+        out = sp.append(out, rate)
     return out
 
 
-def steady_state(gate,bias,n_set):
-    p = probability_calc(gate,bias,n_set)
-    t = tunnel_vector(gate,bias,n_set)
-    return sp.dot(p,t)
+def steady_state(gate, bias, limit):
+    n_set = vector_base(gate, limit)
+    p = probability_calc(gate, bias, n_set)
+    t = tunnel_vector(gate, bias, n_set)
+    return sp.dot(p, t)
+
 
 ########################################################################################################################
 
-def fetch_data(data_file,file_key):
+def fetch_data(data_file, file_key):
     try:
-        fetched_data = pd.DataFrame(pd.read_hdf(data_file,file_key,mode='r'))
+        fetched_data = pd.DataFrame(pd.read_hdf(data_file, file_key, mode='r'))
     except:
         fetched_data = pd.DataFrame(columns={Gate_name, Bias_name, States_name, Thermal_E_name,
                                              Leak_name, Super_Con_name, Current_name})
     return fetched_data
 
+
+# Method that checks if a data-point with the same parameters exist in the set from the datafile
 def data_query(data_point, existing_data):
     point_locations = existing_data[
         (existing_data[Gate_name] == data_point[Gate_name]) &
@@ -84,10 +95,11 @@ def data_query(data_point, existing_data):
         (existing_data[Leak_name] == data_point[Leak_name]) &
         (existing_data[Charge_E_name] == data_point[Charge_E_name]) &
         (existing_data[Super_Con_name] == data_point[Super_Con_name])
-     ]
+        ]
     return point_locations
 
 
+# Method that removes points that have been calculated previously from the list of points to calculate
 def non_existing_points(data_points, existing_data):
     points_to_calc = list(data_points)
     for points in data_points:
@@ -104,6 +116,9 @@ def non_existing_points(data_points, existing_data):
     return points_to_calc
 
 
+# The method used to divide up the calculation of data-points that don't already exist between cores on the computer.
+# The current setting is that the cores used is 2 less then the number of cores available.
+# Calculations happen in batches. At the end of each batch the progress is saved to the main datafile
 def calculate_points(points_to_calc, data_file, file_key, existing_data):
     if len(points_to_calc) != 0:
         with mp.Manager() as manager:
@@ -113,11 +128,11 @@ def calculate_points(points_to_calc, data_file, file_key, existing_data):
                 processes = []
                 for i in xrange(mp.cpu_count() - 2):
                     try:
-                        p = mp.Process(target=current_point, args=(points_to_calc[point],calculated_points))
+                        p = mp.Process(target=current_point, args=(points_to_calc[point], calculated_points))
                         p.start()
                         processes.append(p)
                         point += 1
-                        print point ,'/',len(points_to_calc)
+                        print point, '/', len(points_to_calc)
                     except:
                         pass
 
@@ -132,16 +147,16 @@ def calculate_points(points_to_calc, data_file, file_key, existing_data):
         print('points exist for this line')
 
 
-def current_point(point,calculated_points):
+def current_point(point, calculated_points):
     su.thermal_energy = point[Thermal_E_name]
     su.leakage = point[Leak_name]
     su.super_conductor = point[Super_Con_name]
     su.unit_charge_energy = point[Charge_E_name]
 
-    current = steady_state(gate=point[Gate_name],bias=point[Bias_name],n_set=point[States_name])
+    current = steady_state(gate=point[Gate_name], bias=point[Bias_name], limit=point[States_name])
 
     complete_point = copy.deepcopy(point)
-    complete_point[Current_name]=current
+    complete_point[Current_name] = current
 
     calculated_points.append(complete_point)
 
@@ -168,11 +183,11 @@ def current_line_plot(line_data_set, existing_data, axis, colour, line_label):
 
 
 # This method will print a table with all the settings that have been tested and are inside the datafile
-def check_tested_settings(file,key):
+def check_tested_settings(file, key):
     fetched_data = pd.DataFrame(pd.read_hdf(file, key, mode='r'))
     del fetched_data[Gate_name]
     del fetched_data[Current_name]
-    wanted_info = [Bias_name, States_name, Thermal_E_name, Leak_name, Charge_E_name, Super_Con_name ]
+    wanted_info = [Bias_name, States_name, Thermal_E_name, Leak_name, Charge_E_name, Super_Con_name]
 
     fetched_data = fetched_data.drop_duplicates(subset=wanted_info, keep='first')
 
--- Time_Independent/Gate_comp.py
+++ Time_Independent/Gate_comp.py
@@ -2,17 +2,32 @@ import Calculation_Methods_SS as ss
 import setup_file as su
 import scipy as sp
 import matplotlib.pyplot as plt
+import copy
 
 file = 'SteadyStateOut.h5'
 key = 'current'
 
 input = ss.create_datapoint_dict_steady(
-    gate=su.rounded_linspace(-1, 1, 7),
+    gate=su.rounded_linspace(0, 1, 7),
     bias=su.rounded_linspace(-5, 5, 1001),
     n_set=sp.arange(-7, 8),
     temp=1e-2,
     leak=1e-4,
+    charge_energy=1,
     super=True
 )
 
+
+Existing_Data = ss.fetch_data(file, key)
+
+Gate = input[ss.Gate_name]
+Bias = input[ss.Bias_name]
+
+desired_points = []
+for gate in Gate:
+    for bias in Bias:
+        data_point = copy.deepcopy(input)
+        data_point[ss.Gate_name] = gate
+        data_point[ss.Bias_name] = bias
+        desired_points.append(data_point)
 
